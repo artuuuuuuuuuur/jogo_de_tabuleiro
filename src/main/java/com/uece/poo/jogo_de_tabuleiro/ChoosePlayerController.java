b4eb46d81144fde8596addf53d5de13ff62c6630
Artur de Maria <arturdemaria7@gmail.com>
2025-10-18 04:20:29 -0300

    feature/connect-to-gui: routing pages

--- src/main/java/com/uece/poo/jogo_de_tabuleiro/ChoosePlayerController.java
+++ src/main/java/com/uece/poo/jogo_de_tabuleiro/ChoosePlayerController.java
@@ -10,13 +10,23 @@ import com.uece.poo.jogo_de_tabuleiro.model.JogadorNormal;
 import com.uece.poo.jogo_de_tabuleiro.model.Tabuleiro;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class ChoosePlayerController {
+    @FXML
+    private Button voltarButton;
+
+    public void carregar(boolean modoDebug, int quantidadeDeJogadores) {
+        System.out.println(modoDebug + ", " + quantidadeDeJogadores);
+    }
 
     public void buildTabletop(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/uece/poo/jogo_de_tabuleiro/tabuleiro.fxml"));
@@ -46,6 +56,15 @@ public class ChoosePlayerController {
         Tabuleiro tabuleiro = new Tabuleiro(jogadores);
         tabuleiroController.carregarTabuleiro(tabuleiro, jogadores, true);
 
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    
+    
+    public void voltar(Event event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/uece/poo/jogo_de_tabuleiro/home.fxml"));
+        Parent root = loader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
--- src/main/java/com/uece/poo/jogo_de_tabuleiro/HomeController.java
+++ src/main/java/com/uece/poo/jogo_de_tabuleiro/HomeController.java
@@ -1,12 +1,73 @@
 package com.uece.poo.jogo_de_tabuleiro;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
 
-public class HomeController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+public class HomeController implements Initializable {
 
     @FXML
-    private void switchToChoosePlayer() throws IOException {
-        App.setRoot("choose_player");
+    private ComboBox quantJogadoresComboBox;
+    @FXML
+    private Button modoNormalButton;
+    @FXML
+    private Button modoDebugButton;
+
+    private ObservableList quantJogadoresList = FXCollections.observableArrayList();
+    private boolean modoDebug;
+
+    public void buildChoosePlayer(Event event) throws IOException {
+        int valor = Integer.valueOf(quantJogadoresComboBox.getValue().toString());
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/uece/poo/jogo_de_tabuleiro/choose_player.fxml"));
+        Parent root = loader.load();
+        ChoosePlayerController choosePlayerController = loader.getController();
+
+        choosePlayerController.carregar(modoDebug, valor);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (int i = 2; i <= 6; i++) {
+            quantJogadoresList.add(i);
+        }
+        quantJogadoresComboBox.setItems(quantJogadoresList);
+        modoDebugButton.setOnAction(e -> {
+            modoDebug = true;
+            try {
+                buildChoosePlayer(e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+        modoNormalButton.setOnAction(e -> {
+            modoDebug = false;
+            try {
+                buildChoosePlayer(e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
     }
 }
