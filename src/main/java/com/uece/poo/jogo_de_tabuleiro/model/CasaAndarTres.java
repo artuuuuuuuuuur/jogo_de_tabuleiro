4e5b5cf2e76351e89c0dd63520bf801f666f9657
Artur de Maria <arturdemaria7@gmail.com>
2025-12-02 16:13:50 -0300

    refactor: wrapping conditionals

--- src/main/java/com/uece/poo/jogo_de_tabuleiro/model/CasaAndarTres.java
+++ src/main/java/com/uece/poo/jogo_de_tabuleiro/model/CasaAndarTres.java
@@ -2,7 +2,6 @@ package com.uece.poo.jogo_de_tabuleiro.model;
 
 import java.util.List;
 
-
 public class CasaAndarTres extends Casa {
 
     public CasaAndarTres(int index, List<Jogador> jogadores) {
@@ -12,11 +11,17 @@ public class CasaAndarTres extends Casa {
     @Override
     public void executarAcaoEspecial(Tabuleiro tabuleiro) {
         for (Jogador jogador : jogadores) {
-            if (jogador != null && !(jogador instanceof JogadorAzarado) && jogador.getPosicao() == index) {
-                    jogador.andar(3);
-                    System.out.println(jogador.getNome() + " andou 3 casas.");
+            if (isTarget(jogador)) {
+                jogador.andar(3);
+                System.out.println(jogador.getNome() + " andou 3 casas.");
             }
         }
+    }
+
+    private boolean isTarget(Jogador jogador) {
+        return jogador != null
+                && !(jogador instanceof JogadorAzarado)
+                && jogador.getPosicao() == index;
     }
 
 }
